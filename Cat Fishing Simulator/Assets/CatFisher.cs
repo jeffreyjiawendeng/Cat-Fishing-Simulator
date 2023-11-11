702156075128965565b66029c4655cf6de9b4f49
jeffreyjiawendeng <jeffreyjiawendeng@gmail.com>
2023-11-11 15:32:31 -0500

    Proper Death Time

--- Cat Fishing Simulator/Assets/CatFisher.cs
+++ Cat Fishing Simulator/Assets/CatFisher.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CatFisher : MonoBehaviour
 {
-    static int fishCatched;
-    static int population;
-
+    public static int totalFishCatched = 0;
+    public static int population = 0;
+    //public GameController g;
+    private int timeToFish;
+    private int age;
+    private int lifeExpectancy;
     void Start()
     {
-        population = 100;
+        //g = FindObjectOfType<GameController>().GameController;
+        population++;
+        timeToFish = Random.Range(10, 15);
+        age = 0;
+        lifeExpectancy = Random.Range(8, 10);
         for (int i = 0; i < population; i++)
         {
 
@@ -18,12 +26,20 @@ public class CatFisher : MonoBehaviour
 
     void Update()
     {
-        
+        if (Time.time > 5 * Random.Range(1, 2))
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     void catchFish()
     {
-        fishCatched++;
+        totalFishCatched++;
+    }
+
+    void changePopulation(int fish)
+    {
+
     }
 
     private void OnBecameInvisible()
