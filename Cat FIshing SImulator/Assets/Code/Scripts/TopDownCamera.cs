4af4532fdeab23cfdcc4778e6cfe685a1a929e22
joshuagu789 <joshuagu789@gmail.com>
2023-11-11 15:44:17 -0500

    Revert "Camera controls partly fixed"
    This reverts commit f92a20527b9553d227f6355d16486334adac4171.

--- Cat FIshing SImulator/Assets/Code/Scripts/TopDownCamera.cs
+++ Cat FIshing SImulator/Assets/Code/Scripts/TopDownCamera.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
-using static UnityEngine.GridBrushBase;
-using static UnityEngine.UI.Image;
 
 public class TopDownCamera : MonoBehaviour
 {
@@ -21,27 +18,22 @@ public class TopDownCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Horizontal Movement
         if (Input.GetKey(KeyCode.W))
         {
-            ////transform.Translate(transform.forward * cameraSpeed * Time.deltaTime);
-            //print(transform.eulerAngles.y);
-            //transform.Translate(Quaternion.AngleAxis(transform.eulerAngles.y * -1, Vector3.up) * Vector3.forward * cameraSpeed * Time.deltaTime);
-            transform.Translate(transform.forward * cameraSpeed * Time.deltaTime);
+            transform.Translate(Quaternion.AngleAxis(transform.rotation.y, Vector3.up) * Vector3.forward * cameraSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-transform.right * cameraSpeed * Time.deltaTime);
+            transform.Translate(Quaternion.AngleAxis(transform.rotation.y - 90, Vector3.up) * Vector3.forward * cameraSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(-transform.forward * cameraSpeed * Time.deltaTime);
+            transform.Translate(Quaternion.AngleAxis(transform.rotation.y + 180, Vector3.up) * Vector3.forward * cameraSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(transform.right * cameraSpeed * Time.deltaTime);
+            transform.Translate(Quaternion.AngleAxis(transform.rotation.y + 90, Vector3.up) * Vector3.forward * cameraSpeed * Time.deltaTime);
         }
-        // Vertical Movement
         if (Input.GetKey(KeyCode.E))
         {
             transform.Translate(Vector3.up * cameraSpeed * Time.deltaTime);
@@ -50,7 +42,6 @@ public class TopDownCamera : MonoBehaviour
         {
             transform.Translate(Vector3.down * cameraSpeed * Time.deltaTime);
         }
-        // Camera Rotation
         if (Input.GetKey(KeyCode.UpArrow))
         {
             camera.transform.Rotate(new Vector3(-cameraSpeed * 2 * Time.deltaTime,0,0));
@@ -59,13 +50,13 @@ public class TopDownCamera : MonoBehaviour
         {
             camera.transform.Rotate(new Vector3(cameraSpeed * 2 * Time.deltaTime, 0, 0));
         }
-        //if (Input.GetKey(KeyCode.LeftArrow))
-        //{
-        //    transform.Rotate(Vector3.up * -2 * cameraSpeed * Time.deltaTime);
-        //}
-        //if (Input.GetKey(KeyCode.RightArrow))
-        //{
-        //    transform.Rotate(Vector3.up * 2 * cameraSpeed * Time.deltaTime);
-        //}
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            camera.transform.Rotate(new Vector3(0, -cameraSpeed * 2 * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            camera.transform.Rotate(new Vector3(0, cameraSpeed * 2 * Time.deltaTime, 0));
+        }
     }
 }
