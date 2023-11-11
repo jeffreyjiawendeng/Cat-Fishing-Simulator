4884e288413f6f3bf454936a2751cde62d46d60b
jeffreyjiawendeng <jeffreyjiawendeng@gmail.com>
2023-11-11 15:15:10 -0500

    Fisher Script

--- Cat Fishing Simulator/Assets/CatFisher.cs
+++ Cat Fishing Simulator/Assets/CatFisher.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class CatFisher : MonoBehaviour
 {
-    // Start is called before the first frame update
+    static int fishCatched;
+    static int population;
+
     void Start()
+    {
+        population = 100;
+        for (int i = 0; i < population; i++)
+        {
+
+        }
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void catchFish()
+    {
+        fishCatched++;
+    }
+
+    private void OnBecameInvisible()
     {
         
     }
