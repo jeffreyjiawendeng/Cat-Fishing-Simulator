fed31a8ef934e32fbbd154c85673acda8ffe5346
CrackerO2 <105069353+CrackerO2@users.noreply.github.com>
2023-11-12 10:31:01 -0500

    Final edits

--- Cat FIshing SImulator/Assets/Code/Scripts/SceneLoader.cs
+++ Cat FIshing SImulator/Assets/Code/Scripts/SceneLoader.cs
@@ -5,16 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public GameObject tutorialScreen;
+
     // Start is called before the first frame update
     void Start()
     {
+        tutorialScreen.SetActive(false);
         Time.timeScale = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        closeTut();
     }
 
     public void play()
@@ -35,5 +38,18 @@ public class SceneLoader : MonoBehaviour
     public void restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void openTut()
+    {
+        tutorialScreen.SetActive(true);
+    }
+
+    public void closeTut()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            tutorialScreen.SetActive(false);
+        }
     }
 }
