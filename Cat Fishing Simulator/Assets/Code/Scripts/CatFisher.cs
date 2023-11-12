288e814dae51b3277dad0a4d8f3c0c72b4289938
joshuagu789 <joshuagu789@gmail.com>
2023-11-11 22:04:55 -0500

    First half of fixing cat/fish multiplier

--- Cat Fishing Simulator/Assets/Code/Scripts/CatFisher.cs
+++ Cat Fishing Simulator/Assets/Code/Scripts/CatFisher.cs
@@ -10,15 +10,17 @@ public class CatFisher : MonoBehaviour
     public int timeToFish;
     private int tries;
     public int fishCaught;
+    public int fishToReproduce;
     private int birthYear;
     public int deathYear;
     void Start()
     {
         population++;
         g = FindObjectOfType<GameController>().GetComponent<GameController>();
-        timeToFish = Random.Range(2, 6);
+        timeToFish = Random.Range(1, 4);
         birthYear = g.year;
         deathYear = birthYear + (int)Random.Range(8, 11);
+        fishToReproduce = (int)Random.Range(1, 4);
         fishCaught = 0;
         tries = 0;
         InvokeRepeating("catchFish", 0.0f, 1.0f);
