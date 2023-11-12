026d6e8bc79145aaff44129edc06e4c134692d2d
joshuagu789 <joshuagu789@gmail.com>
2023-11-11 19:41:11 -0500

    Created cat spawner

---
+++ Cat FIshing SImulator/Assets/Code/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+/*
+ * Script that instantiates cats a random distance & direction from the pond as long as they are between the two assigned distances
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cat;
+
+    public void SpawnCat(float minDistance, float maxDistance)
+    {
+        Vector3 location = Quaternion.AngleAxis(Random.Range(0, 360), Vector3.up) * Vector3.forward * Random.Range(minDistance, maxDistance);
+        RaycastHit hit;
+
+        // Attempting to place the cat directly on top of the terrain so that the nav mesh agent is on the nav mesh surface
+        if (Physics.Raycast(location + Vector3.down * 1000, transform.up, out hit) || Physics.Raycast(location + Vector3.up * 1000, -transform.up, out hit))
+        {
+            if (hit.collider.gameObject.tag.Equals("Ground"))
+                Instantiate(cat, hit.point, cat.transform.rotation);
+            else if (minDistance > 0 && maxDistance > 0)    // Recursively spawning a cat if the raycast missed
+                SpawnCat(minDistance - 5, maxDistance - 5);
+        }
+    }
+}
